43ad08925640f8ff8ca2c70ae861aeacd5b49fe0
ashley <ashley.r.boswell@gmail.com>
2018-12-07 13:21:37 +0100

    Remove unused array constructor.

--- PokerHands/src/main/java/com/pokerhands/core/aggregates/PokerHand.java
+++ PokerHands/src/main/java/com/pokerhands/core/aggregates/PokerHand.java
@@ -3,31 +3,21 @@ package com.pokerhands.core.aggregates;
 import com.pokerhands.core.entities.Card;
 import com.pokerhands.core.exceptions.PolkerHandInitialisationException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class PokerHand {
 
     private static final int NUMBER_OF_CARDS = 5;
     private Set<Card> cards;
 
-    public PokerHand(Card... cards) {
-        initialise(Arrays.asList(cards));
-    }
-
     public PokerHand(String cardDescription) {
-        if ((cardDescription.length() % 2) != 0) {
-            throw new PolkerHandInitialisationException(
-                    "Cards must be described in a pair of characters. The first character of the card is the number and the second is the suit");
-        }
-        cardDescription = cardDescription.toUpperCase();
-        List<Card> cards = new ArrayList<>();
-        for (int i = 0; (i + 1) < cardDescription.length(); i = i + 2) {
-            cards.add(new Card(cardDescription.charAt(i), cardDescription.charAt(i + 1)));
-        }
-        initialise(cards);
+        this(parseCardDescriptionString(cardDescription));
     }
 
-    private void initialise(List<Card> cards) {
+    public PokerHand(List<Card> cards) {
         if (cards.size() != NUMBER_OF_CARDS) {
             throw new PolkerHandInitialisationException("A hand must contain " + NUMBER_OF_CARDS + " cards");
         }
@@ -41,6 +31,19 @@ public class PokerHand {
     }
 
     public Set<Card> getCards() {
+        return cards;
+    }
+
+    private static List<Card> parseCardDescriptionString(String cardDescription) {
+        if ((cardDescription.length() % 2) != 0) {
+            throw new PolkerHandInitialisationException(
+                    "Cards must be described in a pair of characters. The first character of the card is the number and the second is the suit");
+        }
+        cardDescription = cardDescription.toUpperCase();
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; (i + 1) < cardDescription.length(); i = i + 2) {
+            cards.add(new Card(cardDescription.charAt(i), cardDescription.charAt(i + 1)));
+        }
         return cards;
     }
 
